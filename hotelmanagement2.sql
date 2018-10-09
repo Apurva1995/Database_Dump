383607acda62a601d60d7a7492c17791cd43e6df
apurvk_zycus <apurv.kumar@zycus.com>
2018-10-09 19:59:16 +0530

    Second database dump

---
+++ hotelmanagement2.sql
@@ -0,0 +1,90 @@
+-- MySQL dump 10.13  Distrib 5.5.34, for Win64 (x86)
+--
+-- Host: localhost    Database: hotelmanagement
+-- ------------------------------------------------------
+-- Server version	5.5.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room` (
+  `number` varchar(10) NOT NULL,
+  `type` varchar(15) NOT NULL,
+  `capacity` varchar(6) NOT NULL,
+  `bookedFrom` datetime DEFAULT NULL,
+  `bookedTill` datetime DEFAULT NULL,
+  `available` int(11) NOT NULL,
+  `userAadhar` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`number`),
+  KEY `fk_user` (`userAadhar`),
+  CONSTRAINT `room_ibfk_1` FOREIGN KEY (`userAadhar`) REFERENCES `user` (`aadharNumber`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES ('1C-179','Super Delux','3Bs',NULL,NULL,1,NULL),('1C-180','Super Delux','3Bs',NULL,NULL,1,NULL);
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `aadharNumber` varchar(12) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `password` varchar(24) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `mobile` decimal(10,0) NOT NULL,
+  `sex` varchar(10) NOT NULL,
+  `age` decimal(10,0) NOT NULL,
+  `isAdmin` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`aadharNumber`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `mobile` (`mobile`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('12345678912','Apurva','Apurva#14','apurvakmr14@gmail.com',7749994776,'Male',23,0),('567890123456','Jhon','Jhon@1234','jhon14@gmail.com',8292437783,'Male',45,0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-09 19:57:03
